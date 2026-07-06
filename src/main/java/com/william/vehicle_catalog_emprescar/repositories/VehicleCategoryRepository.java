df8ce8fd8247fc5e20ebf11f0746768337f5c37c
wkoju84 <koju_william@hotmail.com>
2026-07-06 20:50:50 -0300

    feat(catalog): add VehicleCategoryRepository repositories

---
+++ src/main/java/com/william/vehicle_catalog_emprescar/repositories/VehicleCategoryRepository.java
@@ -0,0 +1,11 @@
+package com.william.vehicle_catalog_emprescar.repositories;
+
+import com.william.vehicle_catalog_emprescar.domain.VehicleCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VehicleCategoryRepository extends JpaRepository<VehicleCategory, UUID> {
+}
