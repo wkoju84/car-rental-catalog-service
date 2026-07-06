f71b192495e22c64564ee879c0dab95be561d684
wkoju84 <koju_william@hotmail.com>
2026-07-06 20:31:56 -0300

    feat(catalog): add VehicleCategory domain

---
+++ src/main/java/com/william/vehicle_catalog_emprescar/domain/VehicleCategory.java
@@ -0,0 +1,25 @@
+package com.william.vehicle_catalog_emprescar.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "vehicle_categories")
+@Getter
+@Setter
+@NoArgsConstructor
+public class VehicleCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String description;
+}
