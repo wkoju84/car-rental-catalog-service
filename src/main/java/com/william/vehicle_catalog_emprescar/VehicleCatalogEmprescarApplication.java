0a6bdefad75997a927e1db84ad3a5cfc4afd105d
wkoju84 <koju_william@hotmail.com>
2026-07-06 19:31:05 -0300

    chore(catalog): add initial configurations

---
+++ src/main/java/com/william/vehicle_catalog_emprescar/VehicleCatalogEmprescarApplication.java
@@ -0,0 +1,13 @@
+package com.william.vehicle_catalog_emprescar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleCatalogEmprescarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleCatalogEmprescarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/william/vehicle_catalog_emprescar/VehicleCatalogEmprescarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.vehicle_catalog_emprescar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleCatalogEmprescarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
